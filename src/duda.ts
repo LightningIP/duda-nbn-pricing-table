d53544db3b23c577849d35bba93d7c1744592dd3
Gavin J <gavinj@outlook.com.au>
2025-06-14 15:04:00 +1000

    Update duda.ts

--- src/duda.ts
+++ src/duda.ts
@@ -15,7 +15,8 @@ import { App as IApp, createApp } from 'vue'
 
 let app: IApp<Element> | null = null
 export function init({ container, props = {} }: { container?: any, props?: {} } = {}) {
-  app = createApp(App);
+  console.log('[LIP_DUDA_NBN_PRICING_TABLE] Init widget with props:', props);
+  app = createApp(App, props);
   registerPlugins(app)
   app.mount(container)
 }
@@ -30,5 +31,5 @@ export function clean() {
     init: init,
     clean: clean,
   };
-  (global as any).dmAPI.registerExternalWidget('pricing-table', handler)
+  (global as any).dmAPI.registerExternalWidget('nbn-pricing-table', handler)
 }(window));
